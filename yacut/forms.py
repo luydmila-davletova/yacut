cbc578013cf0e7a713dc7cfbcd10cc51a14a67af
Людмила Давлетова <ludmiladavletova@192.168.1.3>
2023-07-10 13:18:35 +0300

    v1

---
+++ yacut/forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, URLField
+from wtforms.validators import URL, DataRequired, Length, Optional, Regexp
+
+DESCRIPTION_URL = 'Длинная ссылка'
+MISSING_DATA = 'Обязательное поле'
+ERROR_URL = 'Некорректный URL'
+DESCRIPTION_SHORT = 'Ваш вариант короткой ссылки'
+ERROR_LEN = 'Длина ссылки не может быть больше 16 символов'
+PATTERN_SHORT_URL = r'^[A-Za-z0-9_]+$'
+ERROR_SHORT_URL = 'Указано недопустимое имя для короткой ссылки'
+
+
+class URLForm(FlaskForm):
+    original_link = URLField(
+        DESCRIPTION_URL,
+        validators=[DataRequired(message=MISSING_DATA),
+                    URL(require_tld=True, message=ERROR_URL)]
+    )
+    custom_id = URLField(
+        DESCRIPTION_SHORT,
+        validators=[
+            Length(1, 16,
+                   message=ERROR_LEN),
+            Optional(),
+            Regexp(PATTERN_SHORT_URL,
+                   message=ERROR_SHORT_URL)
+        ]
+    )
+    submit = SubmitField('Создать')
---
+++ yacut/views.py
@@ -0,0 +1,34 @@
+from flask import flash, redirect, render_template, request, abort
+
+from . import app, db
+from .forms import URLForm
+from .models import URLMap
+from .utils import get_unique_short
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index_view():
+    form = URLForm()
+    if form.validate_on_submit():
+        custom_id = form.custom_id.data
+        if not custom_id:
+            custom_id = get_unique_short()
+        elif URLMap.query.filter_by(short=custom_id).first():
+            form.custom_id.errors = [f'Имя {custom_id} уже занято!']
+            return render_template('main_page.html', form=form)
+        url_map = URLMap(
+            original=form.original_link.data,
+            short=custom_id,
+        )
+        db.session.add(url_map)
+        db.session.commit()
+        flash(f'Ваша новая ссылка готова: '
+              f'<a href="{request.base_url}{custom_id}">'
+              f'{request.base_url}{custom_id}</a>')
+    return render_template('main_page.html', form=form)
+
+
+@app.route('/<string:short>', methods=['GET'])
+def url_redirect(short):
+    return redirect(
+        URLMap.query.filter_by(short=short).first_or_404().original)
